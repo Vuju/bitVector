93bed76553d715f3e76b16fec397be38d62fb730
julian@vu-online.de <julian@vu-online.de>
2024-06-26 16:20:35 +0200

    Started working on input parsing

--- src/ads_programm.py
+++ src/ads_programm.py
@@ -1,1 +1,20 @@
 # parser script to interpret input file
+
+# todo accept parameters
+
+inputFile = "input.txt"
+outputFile = "output.txt"
+
+with open(inputFile) as f:
+    cmdCount = int(f.readline())
+    bitVectorString = f.readline()
+
+    # todo init bitVector
+
+    for line in f:
+        cmd = line.strip().split(" ")
+        print(cmd)
+        # todo switch over possible commands
+
+
+# optional todo: implement any amount of error handling
