c158a09221f91a6b2d18e9db08ef7829f4acedee
julian@vu-online.de <julian@vu-online.de>
2024-06-26 16:51:12 +0200

    Set up bit-vector skeleton

--- src/MyBitVector.py
+++ src/MyBitVector.py
@@ -1,1 +1,15 @@
 # Class file implementing an advanced bit-vector as explained in the lecture
+
+class myBitVector:
+    def __init__(self, vectorAsString):
+        self.len = len(vectorAsString)
+        self.vector = [char for char in vectorAsString]
+
+    def access(self, index):
+        return self.vector[index]
+
+    def rank(self):
+        pass
+
+    def select(self):
+        pass
--- src/ads_programm.py
+++ src/ads_programm.py
@@ -1,4 +1,7 @@
 # parser script to interpret input file
+import time
+start_time = time.time()
+
 
 # todo accept parameters
 
@@ -15,6 +18,7 @@ with open(inputFile) as f:
         cmd = line.strip().split(" ")
         print(cmd)
         # todo switch over possible commands
+        time.sleep(1)
 
-
+print("RESULT name=julian_vu time={0} space={1}".format(int((time.time() - start_time) / 1000), "dunno"))
 # optional todo: implement any amount of error handling
